644f491a62ddee1a6fd2666e8ea57cc479b16645
Jeffery LAU <jefferylaujeffery@gmail.com>
2026-09-21 16:54:33 +0800

    update

--- backend/routers/api.py
+++ backend/routers/api.py
@@ -176,7 +176,7 @@ def create_occupancy_reading(sensor: OccupancyReadingCreate, x_sensor_key: str |
     i = ""
     j = 0
     RANDOM = random.choices([1, 0], weights=[0.9, 0.1], k=1)[0]
-    MINUS = random.choices([1, 2], weights=[0.9, 0.1], k=1)[0]
+    MINUS = random.choices([1, 2], weights=[0.8, 0.2], k=1)[0]
 
     if isinstance(sensor, OccupancyBuffer):
         for s in sensor.signal:
